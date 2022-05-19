fd703aa52d53e1e61a039b62a90d8b6bee750789
s2100628-Alex <alex.degerman.dev@gmail.com>
2022-05-19 22:23:48 +0300

    add project

---
+++ src/main/java/com/example/apihelsinki/ApihelsinkiApplication.java
@@ -0,0 +1,13 @@
+package com.example.apihelsinki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApihelsinkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApihelsinkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apihelsinki/ApihelsinkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apihelsinki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApihelsinkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
